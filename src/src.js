75941d0181998136a5073409fe79459d241fc176
Inna Ivanova <innainnaivanov@yandex.ru>
2024-05-24 17:55:53 +0400

    add: game switch functionality

--- src/src.js
+++ src/src.js
@@ -8,11 +8,34 @@ import cards from "./cards";
 
 let menuBtn = document.querySelector('.menu-btn');
 let menu = document.querySelector('.menu');
-menuBtn.addEventListener('click', function () {
+let popup = document.querySelector('.wrapperPopup');
+let dontShowAgainCheckbox = document.getElementById('dontShowAgain');
+let closePopupButton = document.getElementById('closePopup');
+closePopupButton.addEventListener('click', closePopup);
+let overlay = document.createElement('div');
+overlay.classList.add('overlay');
+document.body.appendChild(overlay);
+
+
+function toggleMenu() {
     menuBtn.classList.toggle('active');
     menu.classList.toggle('active');
-})
+}
 
+function closeMenu() {
+    menu.classList.remove('active');
+    menuBtn.classList.remove('active');
+}
+
+menuBtn.addEventListener('click', function () {
+    toggleMenu();
+});
+
+document.addEventListener('click', function (e) {
+    if (menu.classList.contains('active') && !menu.contains(e.target) && !menuBtn.contains(e.target)) {
+        toggleMenu();
+    }
+});
 const cardsContainer = document.querySelector('.cardsContainer');
 const cardsMappings = [];
 let cardSections = cards[0];
@@ -40,7 +63,7 @@ function renderSections() {
         cardsItem.innerHTML = `
             <a class="linkCard" href="#section-${i}">
                 <div class="cardsImage cardsImageAction">
-                    <img src="assets/${mapping.items[0].image}"  alt="img">
+                    <img src="assets/${mapping.items[0].image}"  alt="img" width="240px" ">
                 </div>
                 <div class="cardsTitle">${mapping.title}</div>
             </a>
@@ -56,23 +79,48 @@ function renderCards(sectionIndex) {
             let cardsItem = document.createElement('div');
             let audioImageId = `myImageForAudio-${card.word}`;
             let audioId = `myAudio-${card.word}`;
+            let cardsRotateId = `rotateId-${card.word}`;
+            //let containerRotate = `containerRotate-${card.word}`;
+            let cardItemId = `card-${card.word}`;
             cardsItem.classList.add('cardsItem');
+            cardsItem.setAttribute("id", cardItemId)
             cardsItem.innerHTML = `
-                <a class="linkCard">
-                    <div class="cardsImage cardsImageActionOne">
-                        <img  class="imgDescription" src="assets/${card.image}"  alt="img">
+                <div class="cardContent">
+                    <div class="front">
+                        <div class="cardsImage cardsImageActionOne">
+                            <img class="imgDescription" src="assets/${card.image}" alt="img" >
+                        </div>
+                        <div class="bottomInfo">
+                            <div class="soundBtn">
+                                <img id="${audioImageId}" src="assets/img/audio.png" width="30px" height="30px">
+                            </div>
+                            <audio id="${audioId}">
+                                <source src="assets/${card.audioSrc}" type="audio/mp3">
+                            </audio>
+                            <div class="cardsTitleOne">${card.word}</div>
+                            <div class="infoBtn">
+                                <img id="${cardsRotateId}" src="assets/img/rotate.png" width="30px" height="30px">
+                            </div>
+                        </div>
                     </div>
-                    <div class="bottomInfo">
-                    <div class="soundBtn"><img id="${audioImageId}" src="assets/img/audio.png" width="30px" height="30px"></div>
-                    <audio id="${audioId}">
-                    <source src="assets/${card.audioSrc}" type="audio/mp3">
-                     </audio>
-                    <div class="cardsTitleOne">${card.word}</div>
-                    <div class="infoBtn"><img src="assets/img/rotate.png" width="30px" height="30px"></div>
+                    <div class="back">
+                        <div class="cardsImage cardsImageActionOne">
+                            <img class="imgDescription" src="assets/${card.image}" alt="img">
+                        </div>
+                        <div class="bottomInfo">
+                            <div class="cardsTitleOne">${card.translation}</div>
+                            <div class="infoBtn">
+                                <img id="${cardsRotateId}-back" src="assets/img/rotate.png" width="30px" height="30px">
+                            </div>
+                        </div>
                     </div>
-                </a>
+                </div>
             `
             cardsContainer.append(cardsItem);
+            document.getElementById(cardsRotateId).addEventListener('click',
+                () => document.getElementById(cardItemId).classList.add("rotate"));
+            document.getElementById(cardsRotateId + "-back").addEventListener('click',
+                () => document.getElementById(cardItemId).classList.remove("rotate"));
             document.getElementById(audioImageId).addEventListener('click',
                 () => document.getElementById(audioId).play());
         }
@@ -84,4 +132,39 @@ function renderCards(sectionIndex) {
 renderContent()
 window.addEventListener('hashchange', () => {
     renderContent()
+    closeMenu()
 });
+
+function playPopupShouldBeShown() {
+    let hash = window.location.hash;
+    let dontShowPopup = localStorage.getItem('dontShowPopup');
+    return !(dontShowPopup === 'true' || hash.startsWith('#section-'));
+}
+
+function openPopup() {
+    overlay.style.display = 'block';
+    popup.classList.remove('hidden')
+}
+
+function closePopup() {
+    if (dontShowAgainCheckbox.checked) {
+        localStorage.setItem('dontShowPopup', 'true');
+    }
+    overlay.style.display = 'none';
+    popup.classList.add('hidden');
+}
+
+document.getElementById('switchInput').addEventListener('change', function () {
+    if (this.checked) {
+        document.body.classList.add('alternate-background');
+
+        if (playPopupShouldBeShown()) {
+            openPopup()
+        }
+    } else {
+        document.body.classList.remove('alternate-background');
+    }
+});
+
+
+
